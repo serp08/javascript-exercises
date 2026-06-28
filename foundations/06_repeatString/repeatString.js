49bf47257536304269a6cffcd46446d925629e1a
serp-x2 <serpentcat6312@gmail.com>
2026-06-28 15:16:57 -0400

    Finished tasks 6 and 7

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let out = string;
+    for(let i = 1; i < num; i++){
+        out = out.concat("", string);
+    }return out;
 };
 
 // Do not edit below this line
